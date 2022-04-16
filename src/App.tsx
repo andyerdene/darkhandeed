303bafa2933c3107633cfb89b75718e5c65b612e
andyerdene <anidas.com@gmail.com>
2022-04-16 17:51:16 +0800

    darkhan 1

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Routes, Route } from "react-router-dom";
+import Services from "./components/Services";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Prices from "./components/Prices";
+import Main from "./components/Main";
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/prices" element={<Prices />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import "../style/Header.css";
+import "../style/style.css";
+
+import logo from "../images/icons/icons8-apple-logo-48.png";
+
+import { Link } from "react-router-dom";
+type Props = {};
+
+function Header({}: Props) {
+  return (
+    <div className="headerBG">
+      <div className="contents header">
+        <a href="/" id="logo">
+          <img src="main_logo.png" alt="" />
+          <p>Дархан Дээд Сургууль</p>
+        </a>
+        <div id="menu">
+          <ul>
+            <li>
+              <Link to="/">Нүүр Хуудас</Link>
+            </li>
+            <li>
+              <Link to="/services">Services</Link>
+            </li>
+            <li>
+              <Link to="/prices">Prices</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contuct Us</Link>
+            </li>
+            <li>
+              <Link to="/about">About Us</Link>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <button>Big Button</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
